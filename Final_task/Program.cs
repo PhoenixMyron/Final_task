5c8c5df4a5270f1926b549026b831594bc078092
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-30 14:38:58 +0600

    final task programm is done

---
+++ Final_task/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintArray(string[] array )
+{
+    for ( int i = 0; i < array.Length ; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FindElements(string[] array, string[] SecondArray)
+{
+    for ( int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            SecondArray[i] = array[i];
+            Console.Write($"{SecondArray[i]} ");
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Enter array elements: ");
+string? words = Console.ReadLine();
+string[] array = words.Split(' ');
+string[] SecondArray = words.Split(' ');
+Console.WriteLine("Initial array: ");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Final array: ");
+FindElements(array,SecondArray);
